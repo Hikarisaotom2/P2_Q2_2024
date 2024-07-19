16fa5acf88993d1958b6151ed34071ebbe222084
Claudia Cortes <claudiacortes@MacBook-Pro-de-Claudia.local>
2024-07-18 19:45:11 -0600

    Clase 18 julio: Recursion

--- Semana 1/Clase_Repaso_16072024/src/clase_repaso_16072024/Clase_Repaso_16072024.java
+++ Semana 1/Clase_Repaso_16072024/src/clase_repaso_16072024/Clase_Repaso_16072024.java
@@ -5,6 +5,7 @@
 package clase_repaso_16072024;
 
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -22,20 +23,23 @@ public class Clase_Repaso_16072024 {
         3) Ciclicca, repticion 
         */
        
+       
+       /*
+       1. clase prijcipal del main 
+       2. clase inventario 
+        3.clase auto
+       4. clase recur
+       
+       */
        //argumentos 
        /*
        La computadora no hace nada que noostros no le digamos
        */
-       int x= metodo_ejemplo(4,5);
-       
-       prueba test = new prueba(90,"Hola mundo");
-               
-       ArrayList <prueba> arr = new ArrayList<prueba>();
-       
-       
-       arr.add(test);
-       
-        System.out.println(arr);
+      JOptionPane.showMessageDialog(null, "este es mi mensaje", "MENU",0);
+        System.out.println("BUSQUEDA BINARIA");
+        int [] arreglo = {1,4,6,7,9};
+        int resp = busquedaBinaria(arreglo,7,0,arreglo.length-1);
+        System.out.println("la pos en la que esta el numero es: "+resp);
              
     }
     
@@ -79,6 +83,49 @@ public class Clase_Repaso_16072024 {
     
     
     */
+    
+    
+    
+    /*
+    Busq,. Binaria 
+    // 4
+    // 3,5
+    */
+    
+    /*
+    1)debemos tener un arreglo   el numero que vamos a buscar 
+    2) dividir el arreglo a la mitad. y comprara el numero a buscar con la mitad
+        1. creando un arreglo mas pequeno [1,2,3,4,5] -> [1,2,3] [4,5]
+        2. trabajar con dos indices o dos pos: inicio del arreglo temporal o final del arreglo temporal. 
+    3) si el numero es mas grande que la mitad, seguimos buscando en el lado derecho, sino, en el lado izq
+    4) regresar al punto #2 hasta que encontremos el numero o lleguemos al final del arreglo
+    */
+    
+    
+    public static int busquedaBinaria(int [] numeros, int num, int inicial, int fin){
+        /*
+            1. Caso Base; nos ayuda a deterner el ciclo 
+        */
+         int posMitad=  (inicial+fin)/2;
+        if (fin<inicial) {
+            return -1; 
+        }
+        /*2. Caso Recursivo: nos ayuda a seguirgenerando el ciclo*/
+        if(num<numeros[posMitad]){ // debemos seguir buscando en la izq
+            // parte izq: inicio.....mitad       valores de inicioy fin de la mitad izq
+            return busquedaBinaria(numeros,num,inicial,posMitad-1);
+            
+        } else if (num>numeros[posMitad]){ // debemos seguir buscando en la derecha
+             // parte derecha: mitad.....final 
+             return busquedaBinaria(numeros,num,posMitad+1,fin);
+            
+        }else{ //el numero es igual al valor que esta en la mitad 
+            return posMitad;
+        }
+        
+    }
+    
+    
     
 
     
